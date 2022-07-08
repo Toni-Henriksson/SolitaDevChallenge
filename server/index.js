4f1b4198591ed6e784b5858228ed75efb0922206
Toni-Henriksson <henriksson.toni@outlook.com>
2022-07-08 12:29:13 +0300

    Starter files & setup

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 const express = require("express")
 const app = express();
 const mongoose = require('mongoose')
-mongoose.connect("mongodb+srv://tonba:Asted9292@cluster0.b5ghr.mongodb.net/SolitaHaaste2022?retryWrites=true&w=majority")
+require("dotenv").config();
+
+mongoose.connect(process.env.MONGODB_URL);
 
 app.listen(3001, () => {
     console.log("Server running!");
